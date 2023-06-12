58cb97526fc3c271744f1d83744d70c5c71eae84
yuorei <yuorei71@gmail.com>
2023-06-12 20:21:28 +0900

    Dice game: initial revision

---
+++ dice.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func dice() int {
+	fmt.Println("Rolling dice...")
+	min, max, sum := 1, 6, 0
+
+	for i := 0; i < 2; i++ {
+		result := rand.Intn(max-min) + min // minとmaxが範囲となる
+		sum += result
+		fmt.Printf("Die %d: %d\n", i, result)
+	}
+	return sum
+}
+
+func main() {
+	result := dice()
+	fmt.Printf("Total value: %d\n", result)
+}
